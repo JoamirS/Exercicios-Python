130236de9f9dfe0f6b60b9615691b89b229f4096
JoamirS <jmrsilveira@outlook.com>
2019-05-08 12:31:31 -0300

    Even numbers from 1 to 50
    Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.

---
+++ ex047.py
@@ -0,0 +1,10 @@
+'''
+    Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+'''
+#   Declarando as variáveis
+
+#   Declarando meu laço
+for c in range(1, 51):
+    if c % 2 == 0:
+        print(c)
+print('FIM')
