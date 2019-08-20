e6227c223c10f9ac126e78451aa9a39074fea171
Joamir Silveira <jmrsilveira@outlook.com>
2019-08-20 16:04:28 -0300

    Mathematical Expression Validation

--- ex083.py
+++ ex083.py
@@ -2,20 +2,33 @@
     Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se
     a expressão passada está com os parênteses abertos e fechados na ordem correta.
 """
+HeapList = []
+
 InputMathematicalExpression = str(input('Digite a expressão desejada: '))
+
+for Parentheses in InputMathematicalExpression:
+    if Parentheses == '(':
+        HeapList.append('(')
+    elif Parentheses == ')':
+        if len(HeapList) > 0:
+            HeapList.pop()
+        else:
+            HeapList.append(')')
+            break
 
 PositionFirstOpenParentheses = InputMathematicalExpression.index('(')
 PositionFirstCloseParentheses = InputMathematicalExpression.index(')')
 
 if PositionFirstCloseParentheses < PositionFirstOpenParentheses:
-    print('Sua expressão está errada.')
-
+    print('Você fechou um parenteses antes de abri-lo')
 
 CounterOpenParenthesis = InputMathematicalExpression.count('(')
 CounterCloseParenthesis = InputMathematicalExpression.count(')')
 
 if CounterCloseParenthesis != CounterOpenParenthesis:
-    print('Sua expressão está errada.')
+    print('O número de parenteses abertos e fechados não é igual.')
 
-else:
+if len(HeapList) == 0:
     print('Sua expressão está correta.')
+else:
+    print('Sua expressão está errada.')
