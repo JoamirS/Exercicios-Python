be4ec178ae52e48a121c70df22b8a64e9d4ec937
Joamir Silveira <jmrsilveira@outlook.com>
2020-03-11 09:54:01 -0300

    Grades Function

--- ex106.py
+++ ex106.py
@@ -9,12 +9,20 @@ listAllGrades = list()
 counterTypedValues = 0
 
 
-def grade(totalInput, Great, Worse, Average):
+def grade(totalInput, Great, Worse, Average, situation=False):
     dictionaryAllInformations = dict()
     dictionaryAllInformations['Total de notas digitadas'] = totalInput
     dictionaryAllInformations['A maior nota'] = Great
     dictionaryAllInformations['A menor nota'] = Worse
     dictionaryAllInformations['A média das notas'] = Average
+    if situation is True:
+        if Average >= 7:
+            dictionaryAllInformations['Situação:'] = 'Boa'
+        elif Average >= 5:
+            dictionaryAllInformations['Situação:'] = 'Razoável'
+        else:
+            dictionaryAllInformations['Situação:'] = 'Ruim'
+
     print(dictionaryAllInformations)
 
 
@@ -22,15 +30,26 @@ while stopCondition != 'N':
     inputGradeUser = float(input('Digite sua nota: '))
     print('Deseja continua digitando: Sim ou Não?')
     stopCondition = str(input('')).upper()[0]
+    while stopCondition not in 'SN':
+        print('Erro.\nDeseja continua digitando: Sim ou Não?')
+        stopCondition = str(input('')).upper()[0]
 
     listAllGrades.append(inputGradeUser)
     counterTypedValues += 1
     if stopCondition == 'N':
         break
 
+optionSituation = str(input('Deseja saber sua situação? Sim ou Não')).upper()[0]
+while optionSituation not in 'SN':
+    print('Digite um valor valor válido.')
+    optionSituation = str(input('Deseja saber sua situação? Sim ou Não')).upper()[0]
+if optionSituation == 'S':
+    showSituation = True
+else:
+    showSituation = False
+
 highestValueEntered = max(listAllGrades)
 lowestValueEntered = min(listAllGrades)
 averageGrades = sum(listAllGrades) / len(listAllGrades)
 
-grade(Great=highestValueEntered, Worse=lowestValueEntered, Average=averageGrades, totalInput=counterTypedValues)
-
+grade(Great=highestValueEntered, Worse=lowestValueEntered, Average=averageGrades, totalInput=counterTypedValues, situation=showSituation)
