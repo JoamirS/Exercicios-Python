8fc519fad27cda5d7eaad62a6a1abe72bb5a057d
Joamir Silveira <jmrsilveira@outlook.com>
2023-07-30 20:19:06 -0300

    final commit sum two lists

--- Udemy_02/Class_36.py
+++ Udemy_02/Class_36.py
@@ -7,7 +7,7 @@ Sample:
 list_a = [1, 2, 3, 4, 5, 6, 7]
 list_b = [1, 2, 3, 4]
 
-============Result
+============ Result
 
 list_sum = [2, 4, 6, 8]
 """
@@ -17,7 +17,7 @@ def sum_two_list(list_1, list_2, length):
     list_result_sum_two_list = list()
     counter_position_increase = 0
     while counter_position_increase < length:
-        list_result_sum_two_list.append((list_1[counter_position_increase]) + (list_2[counter_position_increase]))
+        list_result_sum_two_list.append(list_1[counter_position_increase] + list_2[counter_position_increase])
         counter_position_increase += 1
 
     return list_result_sum_two_list
@@ -29,12 +29,17 @@ def check_small_list(list_1, list_2):
 
     if length_list_1 < length_list_2:
         length_small_list = length_list_1
+        small_list = list_1
     else:
         length_small_list = length_list_2
+        small_list = list_2
 
-    return length_small_list
+    return length_small_list, small_list
 
 
 list_a = [1, 2, 3, 4, 5, 6, 7]
-list_b = [1, 2, 3, 4]
-print(sum_two_list(list_a, list_b, 4))
+list_b = [1, 2, 3, 6]
+
+length_small_list_result = check_small_list(list_a, list_b)
+result_sum_list = sum_two_list(list_a, list_b, length=length_small_list_result[0])
+print(result_sum_list)
