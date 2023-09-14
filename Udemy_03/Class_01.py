0a3c11b90684f271ca2fd1f16aa5e2522cbac487
Joamir Silveira <jmrsilveira@outlook.com>
2023-09-13 23:45:18 -0300

    POO

---
+++ Udemy_03/Class_01.py
@@ -0,0 +1,13 @@
+
+class Person:
+    def __init__(self, name, last_name):
+        self.name = name
+        self.last_name = last_name
+
+
+person_01 = Person('Joamir', 'Miranda')
+# person_01.name = 'Luiz'
+# person_01.las_name = 'Miranda'
+
+print(person_01.name)
+print(person_01.last_name)
