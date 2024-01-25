188c8d645bac0a5107113c53212e0e4913201303
Joamir Silveira <jmrsilveira@outlook.com>
2024-01-24 23:36:05 -0300

    import os from itertools import count

--- Udemy_04/Class_09.py
+++ Udemy_04/Class_09.py
@@ -5,11 +5,15 @@ Is a way to permit walk through a structure of directories recursive way.
 To give a tuple sequence, where each tuple has three elements: current directory, a list of subdirectories and a list of
 file the current directory.
 """
+
 import os
 from itertools import count
 
 file_path = os.path.join(r'C:\\Users\\joami\\Documents\\Curso Python\\Python_A-Z')
 counter = count()
+
+print(file_path)
+print(counter)
 
 for root, dirs, files in os.walk(file_path):
     the_counter = next(counter)
