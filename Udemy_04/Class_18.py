0059ca107b038689f402dfc155ff18f875becfba
Joamir Silveira <jmrsilveira@outlook.com>
2024-01-30 23:30:12 -0300

    import random

---
+++ Udemy_04/Class_18.py
@@ -0,0 +1,14 @@
+"""
+random has generators of numbers pseudo aleatory
+"""
+import random
+
+
+r_range = random.randrange(10, 30)
+print(r_range)
+
+r_range_randint = random.randint(0, 50)
+print(r_range_randint)
+
+r_range_uniform = random.uniform(80, 1000)
+print(r_range_uniform)
