628f8ec1d0c46dcfcb2b8838643960b4f23f79dc
Joamir Silveira <jmrsilveira@outlook.com>
2024-10-11 23:55:35 -0300

    About Comments

---
+++ Right_Way/class_03.py
@@ -0,0 +1,9 @@
+# Sample about comments
+
+def calculate_quadratic_equation(j, o, m, r):
+    # Calculate a solution for a quadratic equation
+    # There are always two solutions to a quadratic equation, solution_x1 and solution_x2
+    solution_xr1 = (-o+(o**2-4*j*m)**(1/2))/(2*j)
+    solution_xr2 = (-o-(o**2-4*j*m)**(1/2))/(2*j)
+
+    return solution_xr1, solution_xr2
