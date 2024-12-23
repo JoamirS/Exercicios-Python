dab58d02d92be203937c7bbfc16ee65a9339a6de
Joamir Silveira <jmrsilveira@outlook.com>
2024-12-22 22:11:53 -0300

    validate_input

---
+++ Right_Way/class_13B.py
@@ -0,0 +1,6 @@
+def validate_input(input_user):
+    types_products_input_user_number_tuple = (1, 2, 3)
+    if input_user in types_products_input_user_number_tuple:
+        return True
+    else:
+        return False
