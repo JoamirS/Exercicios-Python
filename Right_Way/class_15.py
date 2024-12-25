0bba10fdcf652ff63b848e4e57703151a61f6f12
Joamir Silveira <jmrsilveira@outlook.com>
2024-12-24 21:50:42 -0300

    exercícios

---
+++ Right_Way/class_15.py
@@ -0,0 +1,31 @@
+# Make a program to read three budgets and show the biggest and smallest
+
+
+# list_budget = [int(input('Digite o primeiro orçamento: ')), int(input('Digite o primeiro orçamento: ')),
+#                int(input('Digite o terceiro orçamento: '))]
+#
+# print('O maior valor é: ')
+# print(max(list_budget, key=int))
+# print('O menor valor é: ')
+# print(min(list_budget, key=int))
+
+# Make a program to ask the price of three products and which one should you buy, knowing that the decision is always
+# for the cheapest
+# list_products = [int(input('Digite o preço do primeiro produto: ')), int(input('Digite o valor do segundo produto: ')),
+#                  int(input('Digite o preço do terceiro produto: '))
+# ]
+# print(f'O valor escolhido foi {min(list_products)}')
+
+# Make a program to read three numbers and display in decreasing order
+# list_numbers = [
+#     int(input('Digite o primeiro número: ')), int(input('Qual é o segundo número: ')),
+#     int(input('Digite o terceiro número: '))
+# ]
+# list_numbers.sort(reverse=True)
+# print(list_numbers)
+
+# Make a program to ask the shift you study. Ask for M - Morning, A - Afternoon, N - Nocturnal
+# Print the message: Good Morning, Good Afternoon, Good Night, inválid value.
+print('M - Manhã | T - Tarde')
+print('Qual é o turno que você está estudando? ')
+
