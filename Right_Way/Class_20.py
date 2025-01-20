f2f106ce0b458aae878a793017e487e26ccc2757
Joamir Silveira <jmrsilveira@outlook.com>
2025-01-19 23:29:04 -0300

    check_if_are_leap

--- Right_Way/Class_20.py
+++ Right_Way/Class_20.py
@@ -3,6 +3,8 @@ You are building a calendar to control work days at the request of HR. In this p
 years are leap year, and which are not to show the calendar in the correct way. Make a program to ask the corresponded
 number of a certain year, right away display if this year is a leap year or not.
 """
+import sys
+
 
 input_year = int(input("Insira um número: "))
 
@@ -16,24 +18,21 @@ def is_multiple_of_4(year):
 
 
 def is_multiple_of_100(year):
-    return not year % 100 == 0
+    return year % 100 == 0
 
 
-# def all_conditions_true() -> bool:
-#     """check if all conditions are true"""
-#     check_all_conditions_leap_year_list = [
-#         is_multiple_of_4(input_year),
-#         is_multiple_of_400(input_year)
-#     ]
-#     return all(check_all_conditions_leap_year_list)
-#
-#
-# def check_if_are_leap():
-#     if all_conditions_met():
-#         print('O ano é bissexto')
-#     else:
-#         print('O ano não é bissexto')
+if is_multiple_of_400(input_year):
+    print(f'O ano {input_year} é bissexto.')
+    sys.exit()
 
+if is_multiple_of_4(input_year) and not is_multiple_of_100(input_year):
+    print(f'O ano {input_year} é bissexto.')
+else:
+    print(f'O ano {input_year} não é bissexto.')
+    sys.exit()
 
-if __name__ == "__main__":
-    check_if_are_leap()
+if is_multiple_of_4(input_year) and is_multiple_of_100(input_year):
+    if is_multiple_of_400(input_year):
+        print(f'O ano {input_year} é bissexto..')
+    else:
+        print(f'O ano {input_year} não é bissexto')
