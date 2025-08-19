ae8db732f0d2b95b82e3800aff0d24ff3324d48e
Joamir Silveira <jmrsilveira@outlook.com>
2025-08-19 08:32:51 -0300

    Exercise with list

---
+++ Right_Way/class_24.py
@@ -0,0 +1,21 @@
+"""
+ Exercises list
+"""
+'''
+Make a program read two strings and display their contents followed your self weight.
+Also report whether the two strings are the same length and have the same or different content.
+'''
+
+first_string = input('Digite a string 1: ')
+second_string = input('Digite a string 2: ')
+
+print(f'String 1: {first_string}')
+print(f'String 2: {second_string}')
+
+print(f"O tamanho de '{first_string}' é: {len(first_string)}")
+print(f"O tamanho de '{second_string}' é: {len(second_string)}")
+
+if len(first_string) == len(second_string):
+    print('As strings possuem o mesmo tamanho.')
+else:
+    print('As strings possuem tamanhos diferentes.')
